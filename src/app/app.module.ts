3e7cb4f55ab9e1707aedd06cff38f20ebfc96909
DimaGlushchenko <aws.inspiration@gmail.com>
2018-02-24 16:35:16 +0200

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,32 +8,35 @@ import { ListPage } from "../pages/list/list";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { GooglePlus } from "@ionic-native/google-plus";
 
 import { HttpModule } from "@angular/http";
+import { ComponentsModule } from "../components/components.module";
 
 import { YoutubeProvider } from "../providers/youtube/youtube";
 import { YoutubeVideoPlayer } from "@ionic-native/youtube-video-player";
-
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { AngularFirestoreModule } from "angularfire2/firestore";
 
 import { environment } from "../environments/environment";
 
-
 @NgModule({
   declarations: [MyApp, HomePage, ListPage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
     HttpModule,
+    ComponentsModule,
+    IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
-    AngularFirestoreModule.enablePersistence() //.enablePersistence() used for offline storage
+    AngularFirestoreModule.enablePersistence()
+    //.enablePersistence() used for offline storage
   ],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage, ListPage],
   providers: [
+    GooglePlus,
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
